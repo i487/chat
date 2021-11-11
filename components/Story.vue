6e6ec3094cddf3ff677b04f82cb28d17eb636ce5
i487 <imronshohhusanov@gmail.com>
2021-11-11 06:59:50 +0500

    add: header

---
+++ components/Story.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="border-[1px] py-[16px]">
+        <ul>
+            <li>
+                <canvas class="canvas" height="83" width="83" style="position: absolute;top: -5px;left: -5px;width: 66px;height: 66px;/* display: none; */"></canvas>
+                <span style="width: 56px; height: 56px;">
+                    <img src="https://picsum.photos/200/200" alt="" class="w-[56px] h-[56px] rounded-full">
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <Header/>
+  <div>
+    <Header/>
+    <div class="container grid grid-cols-12 py-[30px]">
+      <Story class="col-span-8"/>
+    </div>
+  </div>
 </template>
 
 <script>
